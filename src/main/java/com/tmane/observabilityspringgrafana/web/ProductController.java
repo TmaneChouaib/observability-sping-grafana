c361ac33826e88befa4dbce257fcc9aa7deea851
TmaneChouaib <chouaibtmane.ma@gmail.com>
2025-01-26 23:48:35 +0100

    Added Prometheus service to Docker Compose configuration

--- src/main/java/com/tmane/observabilityspringgrafana/web/ProductController.java
+++ src/main/java/com/tmane/observabilityspringgrafana/web/ProductController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestClient;
 
-import java.lang.reflect.Type;
 import java.util.List;
 
 @RestController
